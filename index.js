891d04b5f846fd7303654c5bc2e6a431ae0876e8
Nefla <neflasan@gmail.com>
2015-07-12 16:33:16 -0300

    Remove task creation, only deal with modules.

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var gulp    = require('gulp'),
+    modula  = require('modula-loader'),
+    _       = require('lodash'),
+    plugins = require('gulp-load-plugins')();
 plugins.uglify = require('gulp-uglify');
 plugins.minifycss = require('gulp-minify-css');
 
-var modula = require('modula-loader');
-var taska = require('taska');
-var _ = require('lodash');
 
-function gulpTasks(gulp, path, p) {
+function tasks(gulp, path, p) {
   var $ = _.merge(plugins, p);
-
   var modules = modula('tasks', { gulp: gulp, path: path, $: $ });
-  var build  = taska(modules.build,  createTask);
-  var deploy = taska(modules.deploy, createTask);
-  gulp.task('build', build);
-  gulp.task('deploy', build.concat(deploy));
-
   return modules;
 }
 
-function createTask(name, func){
-  gulp.task(name, func);
-}
-
-module.exports = gulpTasks;
+module.exports = tasks;

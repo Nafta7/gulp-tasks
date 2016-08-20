639d2f4eafeea6a79df0919657ebd6110eb142e1
Nefla <neflasan@gmail.com>
2016-08-20 18:12:51 -0300

    Automatically create individual gulp tasks
     - All tasks will be created automatically
     - Add support to ES6, which is used by default
     - Add option to switch ES6 off and use ES5 instead

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 var gulp    = require('gulp'),
     modula  = require('modula-loader'),
     _       = require('lodash'),
-    plugins = require('gulp-load-plugins')();
-plugins.uglify = require('gulp-uglify');
-plugins.minifycss = require('gulp-minify-css');
+    plugins = require('gulp-load-plugins')()
 
+plugins.uglify = require('gulp-uglify')
+plugins.minifycss = require('gulp-minify-css')
+plugins.path = require('path')
+plugins.gutil = require('gulp-util')
 
-function tasks(gulp, path, p) {
+function taskify(gulp, paths, p, opts){
+  opts = opts || {es6: true}
+
   var $ = _.merge(plugins, p);
-  var modules = modula('tasks', { gulp: gulp, path: path, $: $ });
-  return modules;
+  var modules = modula('tasks', { gulp: gulp, paths: paths, $: $ });
+  modules = filter(modules, opts)
+  var obj = {}
+  _.forOwn(modules, function(value, key){
+    if (_.isFunction(value)){
+      createTask(value, key)
+      obj[key] = key
+    }
+    else if (_.isObject(value)) {
+      obj[key] = _.keys(value)
+    }
+  })
+
+  return obj
+
+  function createTask(func, name){
+    gulp.task(name, function(done){
+      func(done)
+    })
+  }
 }
 
-module.exports = tasks;
+function filter(modules, opts){
+  if (opts.es6) {
+    modules['compile:js'] = modules['compile:es6']
+    modules.build['compile:js'] = modules['compile:es6']
+    delete modules['compile:es6']
+    delete modules.build['compile:es6']
+  }
+  else {
+    delete modules['compile:es6']
+    delete modules.build['compile:es6']
+  }
+  return modules
+}
+
+module.exports = taskify

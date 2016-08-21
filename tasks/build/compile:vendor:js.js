3cd3015605f8ce0ea81ce7df1df34e99dea1fc33
Nefla <neflasan@gmail.com>
2016-08-20 21:26:56 -0300

    Update compile:vendor:js

--- tasks/build/compile:vendor:js.js
+++ tasks/build/compile:vendor:js.js
@@ -3,8 +3,10 @@ module.exports = function(gulp, paths, $){
     var bsync = $.browserSync ? $.browserSync.stream : gutil.noop
     $.gutil.log('Compiling vendor scripts from: ' + $.path.resolve(paths.styles.src))
     var files = paths.vendor.scripts.glob || '**/*'
-    files += '*.js'
-    gulp.src($.path.join(paths.vendor.scripts.src, files))
+    files += '.js'
+    gulp.src($.path.join(paths.vendor.scripts.src, files), {
+      base: paths.vendor.scripts.src
+    })
     .pipe($.concat('vendor.all.js'))
     .pipe(gulp.dest(paths.vendor.scripts.dest))
     .pipe(bsync())
